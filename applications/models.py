95c3d7152c416403384d44870cf75ed7092cd415
jcooter <jcooter@oceanius.com>
2018-03-09 15:26:33 -0500

    Updated package dependencies, fixed broken imports

--- applications/models.py
+++ applications/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.core import urlresolvers
+from django.urls import reverse
 from django.contrib.contenttypes.models import ContentType
 from phonenumber_field.modelfields import PhoneNumberField
 
@@ -57,5 +57,5 @@ class Application(models.Model):
     @property
     def get_admin_url(self):
         content_type = ContentType.objects.get_for_model(self.__class__)
-        return urlresolvers.reverse("admin:%s_%s_change" % (content_type.app_label, content_type.model),
+        return reverse("admin:%s_%s_change" % (content_type.app_label, content_type.model),
                                     args=(self.id,))
